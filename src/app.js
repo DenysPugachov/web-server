757ba037cf9370689ade7755e9fb6764055e7f08
Denys Pugachov <pugachovdenys@gmail.com>
2023-01-04 20:34:35 +0100

    #047 add hbs templates

--- src/app.js
+++ src/app.js
@@ -9,7 +9,32 @@ const app = express()
 const port = 3000
 const publicDirPath = path.join(__dirname, "../public")
 
+
+app.set("view engine", "hbs") // setup "Handlebars templates" (create dynamic pages - reuse code (<footer>, <header>))
 app.use(express.static(publicDirPath))
+
+app.get("", (req, res) => {
+    res.render("index", {
+        "title": "Weather App",
+        "name": "Alice"
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        "title": "Weather App",
+        "name": "Alice",
+        "copyright": "Den_21"
+    })
+})
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        "title": "Weather App",
+        "name": "Alice",
+        "copyright": "Den_21"
+    })
+})
 
 app.get("/weather", (req, res) => {
     res.send([{
