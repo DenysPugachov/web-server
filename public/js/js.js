d518daee9cdae0ac9fde354d5d517d5662e238a5
Denys Pugachov <pugachovdenys@gmail.com>
2023-01-16 20:00:26 +0100

    #59 render message to html

--- public/js/js.js
+++ public/js/js.js
@@ -1,19 +1,28 @@
 const weatherForm = document.querySelector("form")
 const search = document.querySelector("input")
+const messageOne = document.querySelector("#message-1")
+const messageTwo = document.querySelector("#message-2")
+
 
 weatherForm.addEventListener("submit", e => {
     e.preventDefault()
     const searchText = search.value
 
+    messageOne.textContent = "Loading..."
+    messageTwo.style.color = "black"
+    messageTwo.textContent = ""
+
     fetch(`http://localhost:3000/weather?address=${searchText}`).then(response => {
         response.json().then(data => {
             if (data.error) {
-                console.log('data.error :>> ', data.error);
+                messageOne.textContent = ""
+                messageTwo.style.color = "red"
+                messageTwo.textContent = data.error
             } else {
                 const { forecast, location, address } = data[0]
-                console.log('place: >>', location)
-                console.log('address :>> ', address);
-                console.log('forecast :>>', forecast)
+                messageOne.textContent = location + address
+                messageTwo.textContent = forecast
+
             }
         })
     })
