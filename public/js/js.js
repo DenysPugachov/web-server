16fbc77f1f780a4587d51d14d1a9b8733800b766
Denys Pugachov <pugachovdenys@gmail.com>
2023-01-16 19:29:56 +0100

    #58 add input form

--- public/js/js.js
+++ public/js/js.js
@@ -1,14 +1,20 @@
-console.log("Hello form about page")
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
 
-const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Boston.json?access_token=pk.eyJ1IjoiZGVuLTIxIiwiYSI6ImNrdjZvY2hsbjFlYWwybm8wNDZjMDhyamoifQ.vlC2ysnO6PU07Ln1ERWQrg&limit=1`
+weatherForm.addEventListener("submit", e => {
+    e.preventDefault()
+    const searchText = search.value
 
-fetch(url).then(response => {
-    response.json().then(data => {
-        if (data.features.length === 0) {
-            console.log(`Theare is not mathching result found.Please specify search term.`);
-        } else {
-            console.log('Place', data.features[0].place_name)
-            console.log('coordinates :>> ', data.features[0].geometry.coordinates);
-        }
+    fetch(`http://localhost:3000/weather?address=${searchText}`).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                console.log('data.error :>> ', data.error);
+            } else {
+                const { forecast, location, address } = data[0]
+                console.log('place: >>', location)
+                console.log('address :>> ', address);
+                console.log('forecast :>>', forecast)
+            }
+        })
     })
-}) 
+})
